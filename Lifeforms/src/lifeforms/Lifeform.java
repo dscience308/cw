177ad8c6a7539b0ff4858c74cf91ce1d9fe4f258
dscience308 <test@gmail.com>
2015-04-19 15:51:25 -0700

    testMerge1

--- Lifeforms/src/lifeforms/Lifeform.java
+++ Lifeforms/src/lifeforms/Lifeform.java
@@ -1,6 +1,7 @@
 package lifeforms;
 
 
+import java.util.Random;
 import java.util.UUID;
 
 /*
@@ -21,5 +22,23 @@ public class Lifeform {
     int charisma;
     int hitpoints;
     int current_hitpoints;
+    int experience;
+    int level;
     
+    void init()
+    {
+        id = UUID.randomUUID();
+        name = "Joe" + Randomnumber(1,1000); 
+        strength = Randomnumber(1,6)+Randomnumber(1,6)+Randomnumber(1,6);
+        dexterity = Randomnumber(1,6)+Randomnumber(1,6)+Randomnumber(1,6);
+        intelligence = Randomnumber(1,6)+Randomnumber(1,6)+Randomnumber(1,6);
+        charisma = Randomnumber(1,6)+Randomnumber(1,6)+Randomnumber(1,6);
+        hitpoints = Randomnumber(1,10)+Randomnumber(1,10)+Randomnumber(1,10);
+        current_hitpoints = hitpoints;
+    }
+    private int Randomnumber(int min,int max)
+    {
+        Random randomGenerator = new Random();
+        return min + randomGenerator.nextInt(max-min);      
+    }
 }
